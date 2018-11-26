640c2f00049a9e157ee3da66a94dcfaaa549190d
JesusMuley <0619587825@uma.es>
2018-11-26 17:59:46 +0100

    Añado carpeta Fronted y clase Login

--- Frontend/Login.java
+++ Frontend/Login.java
@@ -1,14 +1,42 @@
 package Frontend;
 
-import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class Login extends JPanel {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Login extends JFrame {
+
+	private JPanel contentPane;
 
 	/**
-	 * Create the panel.
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
 	 */
 	public Login() {
-
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 	}
 
 }
