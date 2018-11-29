c1f190983698b589e2d20f7c54f4be9477ae2ed3
marcoasg <trigitos@uma.es>
2018-11-29 16:29:18 +0100

    Añadidas labels a Usuarios

--- Frontend/Usuarios.java
+++ Frontend/Usuarios.java
@@ -13,6 +13,8 @@ import Backend.Usuario;
 import javax.swing.JList;
 import javax.swing.JSpinner;
 import javax.swing.ListSelectionModel;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Usuarios extends JFrame {
 
@@ -42,11 +44,30 @@ public class Usuarios extends JFrame {
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		list.setBounds(10, 11, 794, 96);
 		contentPane.add(list);
+		
+		JLabel label_1 = new JLabel("Datos del usuario ");
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 14));
+		label_1.setBounds(10, 118, 132, 14);
+		contentPane.add(label_1);
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNombre.setBounds(10, 162, 132, 14);
+		contentPane.add(lblNombre);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setFont(new Font("Tahoma",Font.BOLD,14));
+		lblNewLabel.setBounds(136, 118, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel label = new JLabel("");
+		label.setBounds(152, 162, 362, 14);
+		contentPane.add(label);
 		list.addListSelectionListener(new ListSelectionListener() {
 			public void valueChanged(ListSelectionEvent e) {
 				Usuario usuario = new Usuario((String) list.getSelectedValue());
-				if (user.getRol().getNivel() > usuario.getRol().getNivel()) {
-					//mostrar datos del usuario seleccionado
+				if (user.getRol().getNivel() >= usuario.getRol().getNivel()) {
+					lblNewLabel.setText(usuario.getUsuario());
 				}
 				
 			}
