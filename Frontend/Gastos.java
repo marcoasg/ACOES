b3b73c19339fa34ccb52131a37655b9a7e07ba20
JesusMuley <0619587825@uma.es>
2019-01-23 19:53:18 +0100

    Ventana Gastos funcional

--- Frontend/Gastos.java
+++ Frontend/Gastos.java
@@ -6,9 +6,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import Backend.Gasto;
 import Backend.ProyectoLocal;
@@ -16,6 +19,7 @@ import Backend.Usuario;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.JButton;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -32,6 +36,7 @@ public class Gastos extends JFrame {
 	private Usuario user;
 	private Gasto seleccionado;
 	private ProyectoLocal proyecto;
+	private JTextField tEstado;
 	
 	public Gastos(Usuario u, ProyectoLocal pr) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -54,14 +59,39 @@ public class Gastos extends JFrame {
 			new Object[][] {
 			},
 			new String[] {
-				"BENEFICIARIO", "CANTIDAD", "FECHA", "ESTADO"
+				"ID", "BENEFICIARIO", "CANTIDAD", "FECHA", "ESTADO"
 			}
 		));
 		scrollPane.setViewportView(table);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION );
+		table.setRowSelectionAllowed(true);
+	
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(table.getSelectedRow() != -1) {
+					int i = table.getSelectedRow();
+					TableModel modeloSeleccion = table.getModel();
+					seleccionado = new Gasto((int)modeloSeleccion.getValueAt(i, 1));
+					tBeneficiario.setText(seleccionado.getBeneficiario());
+					tCantidad.setText(Float.toString(seleccionado.getCantidad()));
+					SimpleDateFormat formatoDelTexto = new SimpleDateFormat("yyyy-MM-dd");
+					String fecha = formatoDelTexto.format(seleccionado.getFecha());
+					tFecha.setText(fecha);
+					if((boolean) seleccionado.getEstado() == false) {
+						tEstado.setText("Pendiente");
+					}else {
+						tEstado.setText("Aceptado");
+					}
+				}
+			}
+		});
+		
 		
 		for(Gasto g:Gasto.ListaGastos(proyecto)) {
 			rellenarTabla(g);
 		}
+		
 		
 		
 		JLabel lblCantidad = new JLabel("Cantidad:");
@@ -107,21 +137,31 @@ public class Gastos extends JFrame {
 		JButton btnVolver = new JButton("Volver");
 		btnVolver.setBounds(523, 381, 137, 23);
 		contentPane.add(btnVolver);
+		
+		JLabel lblEstado = new JLabel("Estado:");
+		lblEstado.setBounds(42, 463, 46, 14);
+		contentPane.add(lblEstado);
+		
+		tEstado = new JTextField();
+		tEstado.setBounds(123, 460, 277, 20);
+		contentPane.add(tEstado);
+		tEstado.setColumns(10);
 	}
 
 	private void rellenarTabla(Gasto g) {
 		int numCols = table.getModel().getColumnCount();
 		
 		Object[] fila = new Object[numCols];
-		fila[0] = g.getBeneficiario();
-		fila[1] = g.getCantidad();
+		fila[0] = g.getCodigo();
+		fila[1] = g.getBeneficiario();
+		fila[2] = g.getCantidad();
 		SimpleDateFormat formatoDelTexto = new SimpleDateFormat("yyyy-MM-dd");
 		String fecha = formatoDelTexto.format(g.getFecha());
-		fila[2] = fecha;
+		fila[3] = fecha;
 		if(g.getEstado() == false) {
-			fila[3] = "Pendiente";
+			fila[4] = "Pendiente";
 		}else {
-			fila[3] = "Aceptado";
+			fila[4] = "Aceptado";
 		}
 		((DefaultTableModel) table.getModel()).addRow(fila);
 	}
